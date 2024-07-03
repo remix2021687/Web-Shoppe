96c9bffedb3281da0a944a1d85738af2493be49a
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2024-07-03 21:20:52 +0200

    add Footer

--- client/src/Components/Layout/RootLayout/Footer/Footer.jsx
+++ client/src/Components/Layout/RootLayout/Footer/Footer.jsx
@@ -1,5 +1,43 @@
+import { NavLink } from 'react-router-dom'
+import { LinkedinLogo, FacebookLogo, InstagramLogo, TwitchLogo } from '@phosphor-icons/react'
+
 export const Footer = () => {
     return (
-        <h2>Header</h2>
+        <footer className="Footer">
+            <section className="Footer_left">
+                <ul>
+                    <li><NavLink>CONTACT</NavLink></li>
+                    <li><NavLink>TERMS OF SERVICES</NavLink></li>
+                    <li><NavLink>SHIPPING AND RETURNS</NavLink></li>
+                </ul>
+
+                <p><span>© 2021 Shelly.</span> Terms of use <span>and</span> privacy policy.</p>
+            </section>
+            <section className="Footer_right">
+                <label></label>
+                <ul>
+                    <li>
+                        <a href='/' target='_blank'>
+                            <LinkedinLogo size={30} color='#979797'/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='/' target='_blank'>
+                            <FacebookLogo size={30} color='#979797'/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='/' target='_blank'>
+                            <InstagramLogo size={30} color='#979797'/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='/' target='_blank'>
+                            <TwitchLogo size={30} color='#979797'/>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </footer>
     )
 }
