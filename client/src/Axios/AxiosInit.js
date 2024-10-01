0e10d7968fdf5c13d7bfe8e8035551d844a7a61d
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2024-10-02 00:12:08 +0200

    Update AxiosInit.js

--- client/src/Axios/AxiosInit.js
+++ client/src/Axios/AxiosInit.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const AxiosInit = axios.create({
     // baseURL: `http://127.0.0.1:8000/api/`
-    baseURL: `http://localhost:8000/api/`
+    baseURL: import.meta.env.BASE_URL ? import.meta.env.BASE_URL: 'localhost'
 })
 
 AxiosInit.interceptors.request.use((config) => {
